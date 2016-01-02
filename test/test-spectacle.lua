2e2d9cfed95cbde61cebd6a8fb0fe5b8e937f8c5
Jason Foreman <jason@threeve.org>
2016-01-01 23:28:26 -0600

    Add attributes to header test.

--- test/test-spectacle.lua
+++ test/test-spectacle.lua
@@ -4,8 +4,8 @@ local spectacle = require('spectacle')
 local t = {}
 
 function t.testHeader()
-  local result = spectacle.Header(1, "text", {})
-  local expected = "<Header size={1}>text</Header>"
+  local result = spectacle.Header(1, 'text', {id='meh'})
+  local expected = '<Header size={1} id="meh">text</Header>'
   lu.assertEquals(result, expected)
 end
 
